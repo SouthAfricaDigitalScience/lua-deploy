207b1b369ea8a04dc2cfbb0b08d6ea37db0d29b8
Bruce Becker <brucellino@gmail.com>
2016-01-03 18:06:33 +0100

    first commit

---
+++ check-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+# Check build for Lua
+. /etc/profile.d/modules.sh
+module load ci
+cd ${WORKSPACE}/${NAME}-${VERSION}
+make test
+echo $?
+make install # DESTDIR=$SOFT_DIR
+mkdir -p ${REPO_DIR}
+# it's kinda wierd to make a module for lua, when lua is going to be used to replace modules wit lmod, but hey.
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       LUA_VERSION       $VERSION
+setenv       LUA_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(LUA_DIR)/lib
+prepend-path PATH              $::env(LUA_DIR)/bin
+MODULE_FILE
+) > modules/$VERSION
+
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+cp modules/$VERSION ${LIBRARIES_MODULES}/${NAME}
---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+# Lua deploy script
+# this should be run after check-build finishes
+. /etc/profile.d/modules.sh
+echo ${SOFT_DIR}
+module add deploy
+echo ${SOFT_DIR}
+cd ${WORKSPACE}/${NAME}-${VERSION}
+sed -i 's@^INSTALL_TOP.*$@INSTALL_TOP= ${SOFT_DIR}@g' Makefile
+echo "All tests have passed, will now build into ${SOFT_DIR}"
+make install
+echo "Creating the modules file directory ${LIBRARIES_MODULES}"
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/LUA-deploy"
+setenv LUA_VERSION       $VERSION
+setenv LUA_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $LUA_DIR/lib
+prepend-path PATH        $::env(LUA_DIR)/bin
+MODULE_FILE
+) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}
+module add $NAME/$VERSION
+which lua
